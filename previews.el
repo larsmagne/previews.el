72d78f22fff8a547481a94e40c84312a2a75df31
Lars Ingebrigtsen <larsi@gnus.org>
2025-11-12 13:58:04 +0100

    Remove Diamond, since they're now gone

--- previews.el
+++ previews.el
@@ -41,7 +41,7 @@
   (interactive)
   (let ((time (float-time (current-time)))
 	did)
-    (cl-incf time (* 60 60 24 12))
+    (cl-incf time (* 60 60 24 7))
     (unless (file-exists-p (previews-file time))
       (setq did (previews-index time)))
     (when (and did
@@ -59,160 +59,37 @@
 					     (parse-time-string date))))))
 
 (defun previews-index (time)
-  (when-let ((diamond (previews--index-diamond time))
-	     (lunar (seq-uniq (previews--index-lunar time)
+  (when-let ((lunar (seq-uniq (previews--index-lunar time)
 			      (lambda (e1 e2)
 				(equal (cdr (assq :code e1))
 				       (cdr (assq :code e2))))))
 	     (prh (previews--index-prh (format-time-string "%Y-%m" time))))
-    ;; Sort the publishers alphabetically.
-    (setq diamond
-	  (sort (append lunar prh diamond)
-		(lambda (e1 e2)
-		  (cond
-		   ;; Search merch to the end.
-		   ((and (assq :merch e1)
-			 (not (assq :merch e2)))
-		    nil)
-		   ((and (not (assq :merch e1))
-			 (assq :merch e2))
-		    t)
-		   ;; Sort by publisher within non-merch/merch.
-		   (t
-		    (if (equal (downcase (cdr (assq :publisher e1)))
-			       (downcase (cdr (assq :publisher e2))))
-			(string< (cdr (assq :publisher e1))
-				 (cdr (assq :publisher e2)))
-		      (string< (downcase (cdr (assq :publisher e1)))
-			       (downcase (cdr (assq :publisher e2))))))))))
-    (previews-fetch-and-write diamond time)
+    (previews-fetch-and-write
+     ;; Sort the publishers alphabetically.
+     (sort (append lunar prh)
+	   (lambda (e1 e2)
+	     (cond
+	      ;; Search merch to the end.
+	      ((and (assq :merch e1)
+		    (not (assq :merch e2)))
+	       nil)
+	      ((and (not (assq :merch e1))
+		    (assq :merch e2))
+	       t)
+	      ;; Sort by publisher within non-merch/merch.
+	      (t
+	       (if (equal (downcase (cdr (assq :publisher e1)))
+			  (downcase (cdr (assq :publisher e2))))
+		   (string< (cdr (assq :publisher e1))
+			    (cdr (assq :publisher e2)))
+		 (string< (downcase (cdr (assq :publisher e1)))
+			  (downcase (cdr (assq :publisher e2)))))))))
+     time)
     t))
-
-(defun previews--index-diamond (time)
-  (set-locale-environment "C")
-  (with-current-buffer (url-retrieve-synchronously
-			(format "https://www.previewsworld.com/Catalog/CustomerOrderForm/TXT/%s%s"
-				(upcase (format-time-string "%h" time))
-				(format-time-string "%y" time))
-			t t)
-    (goto-char (point-min))
-    (when (and (search-forward "\n\n" nil )
-	       (= (following-char) 255))
-      (decode-coding-region (point) (point-max) 'utf-16))
-    (previews-interpret-index (previews-parse-index))))
-
-(defun previews-parse-index ()
-  (goto-char (point-min))
-  (while (re-search-forward "\r" nil t)
-    (replace-match "" t t))
-  (goto-char (point-min))
-  (while (re-search-forward "^PAGE" nil t)
-    (delete-region (match-beginning 0) (line-beginning-position 2)))
-  (goto-char (point-min))
-  (let ((publisher nil)
-	(merchandise nil)
-	(comics nil))
-    (while (not (eobp))
-      (cond
-       ((looking-at ".*\t")
-	(push (list publisher merchandise
-		    (split-string (buffer-substring-no-properties
-				   (point) (line-end-position))
-				  "\t"))
-	      comics))
-       ((looking-at "MERCHANDISE *$")
-	(setq merchandise t))
-       ((looking-at ".+")
-	(setq publisher (match-string 0))))
-      (forward-line 1))
-    (nreverse comics)))
-
-(defun previews-interpret-index (index)
-  (cl-loop with prev-name
-	   for (publisher merchandise (class code title date price)) in index
-	   ;; Sometimes there's an extra TAB before the title.  In that
-	   ;; case, shift values down.
-	   do (when (zerop (length title))
-		(setq title date
-		      date price
-		      price ""))
-	   when price
-	   collect (let ((data
-			  (copy-sequence
-			   `((:publisher . ,publisher)
-			     (:code . ,(replace-regexp-in-string " " "" code))
-			     (:price . ,(replace-regexp-in-string "SRP: " ""
-								  price))
-			     (:date . ,date)
-			     (:distributor . "Diamond"))))
-			 name)
-		     (when (plusp (length class))
-		       (nconc data (list (cons :class class))))
-		     (when merchandise
-		       (nconc data (list (cons :merch "t"))))
-		     (with-temp-buffer
-		       (insert title)
-		       (goto-char (point-min))
-		       (when (re-search-forward " +(OF \\([0-9]+\\))" nil t)
-			 (nconc data (list (cons :duration (match-string 1))))
-			 (replace-match ""))
-		       (goto-char (point-min))
-		       (when (re-search-forward " +(C: \\([-0-9]+\\))" nil t)
-			 (nconc data (list (cons :comething (match-string 1))))
-			 (replace-match ""))
-		       (goto-char (point-min))
-		       (when (re-search-forward " +(\\([A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9][0-9][0-9]\\))" nil t)
-			 (nconc data (list (cons :original (match-string 1))))
-			 (replace-match ""))
-		       (goto-char (point-min))
-		       (when (re-search-forward " +(RES)" nil t)
-			 (nconc data (list (cons :resolicited t)))
-			 (replace-match ""))
-		       (goto-char (point-min))
-		       (when (re-search-forward " +(MR)" nil t)
-			 (nconc data (list (cons :mature t)))
-			 (replace-match ""))
-		       (goto-char (point-min))
-		       (when (re-search-forward " +(O/A)" nil t)
-			 (replace-match ""))
-		       (goto-char (point-min))
-		       (when (re-search-forward
-			      "\\b\\(HC GN\\|GN\\|TP\\|HC\\|SC\\)\\b"
-			      nil t)
-			 (nconc data (list (cons :binding (match-string 1)))))
-		       (goto-char (point-min))
-		       (cond
-			;; Variants.
-			((looking-at "\\(.*\\) +\\(\\(#[^ ]+\\)\\|\\(.*VOL [^ ]+\\)\\|ONE SHOT\\)")
-			 (nconc data (list (cons :issue (match-string 2))
-					   (cons :title (match-string 1))))
-			 (setq name (match-string 0))
-			 (when (equal name prev-name)
-			   (nconc data (list (cons :variant t))))
-			 (setq prev-name name))
-			;; T-shirts.
-			((looking-at "\\(.* +T/S\\)")
-			 (nconc data (list (cons :title (match-string 1))))
-			 (setq name (match-string 1))
-			 (when (equal name prev-name)
-			   (nconc data (list (cons :variant t))))
-			 (setq prev-name name))
-			;; Hardcovers/softcovers
-			((or
-			  (looking-at "\\(.*\\) \\(LTD ED HC\\|HC GN\\|SGN ED\\)\\b\\(.*?\\)")
-			  (looking-at "\\(.*\\) \\(HC\\|SC\\|GN\\)\\b\\(.*?\\)"))
-			 (setq title (concat (match-string 1)
-					     (or (match-string 3) "")))
-			 (nconc data (list (cons :title title)))
-			 (when (equal title prev-name)
-			   (nconc data (list (cons :variant t))))
-			 (setq prev-name title)))		     
-		       (nconc data (list (cons :name (buffer-string)))))
-		     data)))
 
 (defun previews-fetch-and-write (index time)
   (with-temp-buffer
-    (insert (json-encode (previews-fetch index)))
+    (insert (json-encode index))
     (let ((coding-system-for-write 'utf-8))
       (write-region (point-min) (point-max) (previews-file time))))
   (previews-make-cache)
@@ -230,58 +107,10 @@
     (write-region (point-min) (point-max)
 		  (expand-file-name "timestamp.js" previews-data-directory))))
 
-(defun previews-fetch (index)
-  (cl-loop for elem in index
-	   collect (if (assq :text elem)
-		       ;; If we already have the text, then we don't have
-		       ;; to fetch anything.
-		       elem
-		     ;; Fetch the text from Diamond.
-		     (let ((data (previews-fetch-id (cdr (assq :code elem))))
-			   (shr-base (shr-parse-base
-				      "https://www.previewsworld.com/")))
-		       (message "%s" (cdr (assq :name elem)))
-		       (append elem
-			       `((:img . ,(and (plusp (length (nth 0 data)))
-					       (shr-expand-url (nth 0 data))))
-				 (:creators . ,(nth 1 data))
-				 (:text . ,(string-trim (nth 2 data)))))))))
-
 (defun previews-file (time)
   (expand-file-name
    (format-time-string "previews-%Y-%m.json" time)
    previews-data-directory))
-
-(defun previews-fetch-id (id)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       (format "https://www.previewsworld.com/Catalog/%s"
-	       (replace-regexp-in-string " +" "" id))
-       t t)
-    (goto-char (point-min))
-    (while (re-search-forward "[ \t\r\n]+" nil t)
-      (replace-match " " t t))
-    (prog1
-	(previews-parse-dom
-	 (libxml-parse-html-region (point-min) (point-max)))
-      (kill-buffer (current-buffer)))))
-
-(defun previews-parse-dom (dom)
-  ;; Image.
-  (list (dom-attr (dom-by-id dom "mainContentImage") 'src)
-	;; Creators.
-	(dom-texts (dom-by-class dom "^Creators$"))
-	;; Text.
-	(string-join
-	 (cl-loop for child in (dom-children (dom-by-class dom "^Text$"))
-		  when (or (stringp child)
-			   (eq (dom-tag child) 'i))
-		  collect (if (stringp child)
-			      child
-			    (dom-text child))))
-	;; Price.
-	(car (last (split-string
-		    (dom-texts (dom-by-class dom "^SRP$")))))))
 
 (defun previews-cache-images (month &optional refresh)
   (let ((dir (expand-file-name (format "img/%s" month)
@@ -449,6 +278,8 @@
     (PW "Pow Pow")
     (FB "Fantagraphics Books")
     (DQ "Drawn & Quarterly")
+    (AY "Asylum Press")
+    (UB "Uncivilized Books")
     (AC "Archie Comics")))
 
 (defun previews--lunar-publisher (code)
